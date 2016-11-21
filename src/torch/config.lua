73b9cafa20649ba418c2f0d2af95666797c28c84
Philippe Vinchon <pvinchon@gilt.com>
2016-11-21 17:08:41 +0000

    Generic config fetcher

---
+++ src/torch/config.lua
@@ -0,0 +1,9 @@
+-- Copyright (C) 2016 Pau Carré Cardona - All Rights Reserved
+-- You may use, distribute and modify this code under the
+-- terms of the Apache License v2.0 (http://www.apache.org/licenses/LICENSE-2.0.txt).
+
+--
+-- Default configuration file
+--
+
+return {}
